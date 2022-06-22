7b3fea1935ac3223e265189dc7a20931f4cf7a66
Pewners <jfadikpe2021@elam.co.uk>
2022-06-22 11:30:21 +0100

    Game

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	//void Start()
+	//{
+
+	//}
+
+	// Update is called once per frame
+	//void Update()
+	//{
+
+	//}
+
+	//if arrow key is pressed
+		//if left is pressed
+			//move left
+
+		//if right is pressed
+			//move right
+
+		//if up is pressed
+			//move up
+
+		//if down is pressed
+			//move down
+
+	//if obstacle is onscreen
+		//spawn gameobject obstacle
+
+	//if obstacle is offscreen
+		//destroy gameobject obstacle
+}
