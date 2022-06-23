27069ff78cd21b7ecd616d8fe2fae95c37158fd3
Pewners <jfadikpe2021@elam.co.uk>
2022-06-23 09:57:34 +0100

    added like 2 scripts

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,34 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	// Start is called before the first frame update
-	//void Start()
-	//{
+	
+	void Start()
+	{
 
-	//}
+	}
 
-	// Update is called once per frame
-	//void Update()
-	//{
+	
+	void Update()
+	{
+		//if arrow key is pressed
+			//if left is pressed
+				//move left
 
-	//}
+			//if right is pressed
+				//move right
 
-	//if arrow key is pressed
-		//if left is pressed
-			//move left
+			//if up is pressed
+				//move up
 
-		//if right is pressed
-			//move right
-
-		//if up is pressed
-			//move up
-
-		//if down is pressed
-			//move down
-
-	//if obstacle is onscreen
-		//spawn gameobject obstacle
-
-	//if obstacle is offscreen
-		//destroy gameobject obstacle
+			//if down is pressed
+				//move down
+	}
 }
---
+++ Assets/Vehicles.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn game object every 1 second
+
+        //if game object goes off screen
+            //destroy game object
+    }
+}
