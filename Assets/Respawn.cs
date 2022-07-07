2d761d9d05073c66ab0ae26f7ba9f3dff25b68f6
Pewners <jfadikpe2021@elam.co.uk>
2022-07-07 11:42:40 +0100

    respawn

--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -2,26 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Finish : MonoBehaviour
+public class Respawn : MonoBehaviour
 {
+    public GameObject Player;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "Player")
+        if (col.tag == "End")
         {
-            Debug.Log("Finish");
+            Destroy(Player);
+            Instantiate(Player, new Vector2( 0, -4), Quaternion.identity);
         }
     }
 }
