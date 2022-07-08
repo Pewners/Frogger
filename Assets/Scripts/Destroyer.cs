4f294f9a8721b428b3b2895dbefe4739c626abfd
Pewners <jfadikpe2021@elam.co.uk>
2022-07-08 11:50:48 +0100

    sprites added

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -6,7 +6,7 @@ public class Destroyer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "CleanUp") 
+        if (collision.gameObject.tag == "Enemy") 
         {
             Destroy(collision.gameObject);
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,19 @@ public class PlayerController : MonoBehaviour
 	public float maxX = 4.17f;
 	public float maxY = 4.436f;
 
+	public SpriteRenderer spriteRenderer;
+	public Sprite up;
+	public Sprite down;
+	public Sprite left;
+	public Sprite right;
+	void ChangeSprite(Sprite down)
+	{
+		spriteRenderer.sprite = down;
+	}
 
 	void Start()
 	{
-		//spawn player at start
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 	}
 
 	
@@ -22,39 +31,26 @@ public class PlayerController : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minX)
 		{
-			//Debug.Log("Left");
 			transform.Translate(Vector2.left);
-			//if player > -4.03, -0.0057
-				//dont move
+            ChangeSprite(left);
 		}
 
 		if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxX)
 		{
-			//Debug.Log("Right");
 			transform.Translate(Vector2.right);
+			ChangeSprite(right);
 		}
 
 		if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxY)
 		{
-			//Debug.Log("Up");
 			transform.Translate(Vector2.up);
+			ChangeSprite(up);
 		}
 
 		if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minY)
 		{
-			//Debug.Log("Down");
 			transform.Translate(Vector2.down);
+			ChangeSprite(down);
 		}
-
-		
 	}
-
-	//void OnTriggerEnter2D (Collider2D col)
-			//if player touches vehicle
-				//kill player
-				//lives = lives-1
-				//respawn at start
-
-			//if player touches finish
-				//finish game
 }
